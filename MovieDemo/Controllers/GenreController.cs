e5a8a60eb0bb5d01ff83ff90a34b1a47b140d54d
User <User@WX1129720>
2025-12-29 16:02:14 +0200

    added create genre and small changes

---
+++ MovieDemo/Controllers/GenreController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using MovieDemo.Data;
+using MovieDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieDemo.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class GenresController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public GenresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var genres = await _context.Genres
+                .Include(g => g.Movies)
+                .OrderBy(g => g.Name)
+                .ToListAsync();
+
+            return View(genres);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(genre);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(genre);
+        }
+
+        // --- ADDED DELETE METHODS BELOW ---
+
+        // GET: Genres/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            // We include Movies so the delete page can show how many assets will be unlinked
+            var genre = await _context.Genres
+                .Include(g => g.Movies)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (genre == null) return NotFound();
+
+            return View(genre);
+        }
+
+        // POST: Genres/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre != null)
+            {
+                _context.Genres.Remove(genre);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MovieDemo/Models/MovieList.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieDemo.Models
+{
+    public class MovieList
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+
+    }
+}
